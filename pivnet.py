2f723799c0f7c16640a4de2c3ffc87c046ff9e87
guidowb <guidowb@gmail.com>
2015-09-30 10:58:14 -0500

    Added releases command

--- pivnet.py
+++ pivnet.py
@@ -16,15 +16,40 @@ def pivnet_open(url):
 	request.add_header('Accept', 'application/json')
 	return urllib2.urlopen(request)
 
-def pivnet_products(pattern = ""):
+def pivnet_products(product_pattern = ""):
 	products = json.load(pivnet_open('https://network.pivotal.io/api/v2/products'))
-	products = [p for p in products["products"] if pattern in p["name"]]
+	products = [p for p in products["products"] if product_pattern in p["name"]]
 	return products
 
+def pivnet_select_product(product_pattern):
+	products = pivnet_products(product_pattern)
+	if len(products) < 1:
+		print product_pattern, "does not match any products"
+		sys.exit(1)
+	if len(products) > 1:
+		print product_pattern, "matches multiple products:"
+		print "\n".join(["   " + p["name"] for p in products])
+		print "Please be more specific"
+		sys.exit(1)
+	return products[0]
+
+def pivnet_releases(product_pattern, release_pattern = ""):
+	product = pivnet_select_product(product_pattern)
+	releases = json.load(pivnet_open('https://network.pivotal.io/api/v2/products/' + str(product["id"]) + '/releases'))
+	releases = [r for r in releases["releases"] if release_pattern in r["version"]]
+	return releases
+
 def list_products(argv):
-	pattern = argv[1] if len(argv) > 1 else ""
-	products = pivnet_products(pattern)
+	product_pattern = argv[1] if len(argv) > 1 else ""
+	products = pivnet_products(product_pattern)
 	print "\n".join([p["name"] for p in products])
+
+def list_releases(argv):
+	exit_with_usage(argv) if len(argv) < 2 else None
+	product_pattern = argv[1]
+	release_pattern = argv[2] if len(argv) > 2 else ""
+	releases = pivnet_releases(product_pattern, release_pattern)
+	print "\n".join([r["version"] for r in releases])
 
 def unknown_command(argv):
 	print "unknown command", argv[0]
@@ -34,18 +59,24 @@ def print_help(argv = None):
 	for name, command in commands.iteritems():
 		print command["usage"]
 
-def check_argv(argv):
-	if len(argv) < 2:
+def print_usage(argv):
+	print "Usage:", commands.get(argv[0])["usage"]
+
+def exit_with_usage(argv = None):
+	if argv is None:
 		print_help()
-		sys.exit(1)
+	else:
+		print_usage(argv)
+	sys.exit(1)
 
 commands = {
 	"help":     { "func": print_help,    "usage": "help" },
-	"products": { "func": list_products, "usage": "products [<pattern>]" },
+	"products": { "func": list_products, "usage": "products [<product-name>]" },
+	"releases": { "func": list_releases, "usage": "releases <product-name> [<release-name>]"}
 }
 
 def main(argv):
-	check_argv(argv)
+	exit_with_usage() if len(argv) < 2 else None
 	command = commands.get(argv[1], { "func": unknown_command } )
 	result = command["func"](argv[1:])
 	sys.exit(result)
