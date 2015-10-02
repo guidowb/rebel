1cfb98ea943a020a5e41f72866d743a316c4087a
guidowb <guidowb@gmail.com>
2015-10-01 19:10:19 -0500

    Added support for regions

--- opsmgr.py
+++ opsmgr.py
@@ -5,22 +5,25 @@ import cli
 import sys
 import json
 
-def opsmgr_list_images():
+def opsmgr_list_images(region = None):
 	command = [
 		'ec2',
 		'describe-images',
 		'--filters', 'Name=name,Values=pivotal-ops-manager-v*'
 	]
+	if region is not None:
+		command.extend(['--region', region])
 	return json.loads(aws.aws_cli_verbose(command))["Images"]
 
 def list_images_cmd(argv):
-	images = opsmgr_list_images()
+	region = argv[1] if len(argv) > 1 else None
+	images = opsmgr_list_images(region)
 	images = sorted(images, key=lambda image: image["CreationDate"])
 	images.reverse()
 	print "\n".join([i["ImageId"] + " " + i["Name"] for i in images])
 
 commands = {
-	"images":       { "func": list_images_cmd,    "usage": "images" },
+	"images":       { "func": list_images_cmd,    "usage": "images [<region>]" },
 }
 
 if __name__ == '__main__':
